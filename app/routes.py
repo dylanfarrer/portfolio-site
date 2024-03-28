245fe7ae6ea284c1679f7899bc8841211000fbc9
Dylan Farrer <dylan.farrer.2019@live.rhul.ac.uk>
2024-03-28 16:57:39 +0000

    updated routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,28 +2,10 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__)
 
-"""
-@bp.route('/')
-def index():
-    return render_template('index.html')
-"""
-
 @bp.route('/')
 def my_home():
-    return render_template('index.html')
+    return render_template('about.html')
 
 @bp.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
-
-@bp.route('/submit_form', methods=['POST', 'GET'])
-def submit_form():
-    if request.method == "POST":
-        try:
-            data = request.form.to_dict()
-            #write_to_csv(data)
-            return redirect('/thankyou.html')
-        except:
-            return "did not save to database"
-    else:
-        return 'hmm, something went wrong'
